f4a84f8f0e49b41a7a386bab33e418eb7e966dee
Julian <julianrosenthal@yahoo.com>
2020-11-23 17:55:16 -0800

    commit tree 42149759de6ec08dc6fa787593c76adc6156e927

--- nyc_api.rb
+++ nyc_api.rb
@@ -1,9 +1,8 @@
 require 'net/http'
- require 'open-uri'
- require 'json'
+require 'open-uri'
+require 'json'
  
- class GetPrograms
-
+class GetPrograms
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
   def get_programs
